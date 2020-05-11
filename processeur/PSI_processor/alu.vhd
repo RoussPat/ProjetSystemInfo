5273897b7aa032d3c103886aa70b7480d1ef5859
RoussPat <patrickrouss@gmail.com>
2020-05-11 10:48:19 +0200

    modif de alu et test alu

--- processeur/PSI_processor/alu.vhd
+++ processeur/PSI_processor/alu.vhd
@@ -48,23 +48,25 @@ architecture Behavioral of alu is
 	 
 	--constant size : integer := 8;
 	-- Declaration des operations
-	signal S_add : STD_LOGIC_VECTOR(size downto 0);
+	signal S_add : STD_LOGIC_VECTOR(size-1 downto 0);
+	signal S_add_aux : STD_LOGIC_VECTOR(size downto 0);
 	signal S_sou : STD_LOGIC_VECTOR(size-1 downto 0);
 	signal S_mul : STD_LOGIC_VECTOR(size-1 downto 0);
+	signal S_mul_aux : STD_LOGIC_VECTOR(2*size-1 downto 0);
 	Signal S_div : STD_LOGIC_VECTOR(size-1 downto 0);
 	signal S_equ : STD_LOGIC;
 	signal S_inf : STD_LOGIC;
 	signal S_sup : STD_LOGIC;
-	signal C : STD_LOGIC := '0';
-	signal N : STD_LOGIC := '0';
-	signal Z : STD_LOGIC := '0';
-	signal V : STD_LOGIC := '0';
+	signal C_AUX : STD_LOGIC := '0';
+	signal N_AUX : STD_LOGIC := '0';
+	signal Z_AUX : STD_LOGIC := '0';
+	signal O_AUX : STD_LOGIC := '0';
 	
 begin
 
 -- Implementation des operations
 	-- Gestion des OpCodes
-	S <=  S_add(size downto 0) when Ctrl_Alu = X"1" else
+	S <=  S_add(size-1 downto 0) when Ctrl_Alu = X"1" else
 			S_sou(size-1 downto 0) when Ctrl_Alu = X"3" else
 			S_mul(size-1 downto 0) when Ctrl_Alu = X"2" else
 			(X"00" & S_equ) when Ctrl_Alu = X"B" else
@@ -73,35 +75,41 @@ begin
 			X"00";
 			
 	-- Actions a faire en fonction de l'OpCode
-	S_add <= ("0" & A)+ ("0" & B);
+	S_add_aux <= (B"0" & A) + (B"0" & B);
+	S_add <= S_add_aux(size-1 downto 0);
 	S_sou <= A-B;
-	S_mul <= A*B;
+	S_mul_aux <= A*B;
+	S_mul <= S_mul_aux(7 downto 0);
 	
 	S_equ <= '1' when (Ctrl_Alu = X"B" and A = B) else '0'; -- egalite B
 	S_inf <= '1' when (Ctrl_Alu = X"9" and A < B) else '0'; -- inferieur stricte 9
 	S_sup <= '1' when (Ctrl_Alu = X"A" and A > B) else '0'; -- superieur stricte A
 	
 	-- Flag Carry
-	C <=  S_add(size) when (Ctrl_Alu = X"1" and S_add(size) = '1') else '0'; 
+	C_AUX <=  S_add_aux(size) when (Ctrl_Alu = X"1" and S_add_aux(size) = '1') else '0'; 
 	
 	-- Flag Negative
-	N <=  S_add(size-1) when (Ctrl_Alu = X"1" and S_add(size-1) = '1') else	-- addition 1
-			S_sou(size-1) when (Ctrl_Alu = X"3" and S_sou(size-1) = '1') else	-- soustraction 3
-			S_mul(size-1) when (Ctrl_Alu = X"2" and S_mul(size-1) = '1') else	-- multiplication 2
-			'0';
+	N_AUX	<=	S_sou(size-1) when (Ctrl_Alu = X"3" and S_sou(size-1) = '1') else	-- soustraction 3
+				S_mul(size-1) when (Ctrl_Alu = X"2" and S_mul(size-1) = '1') else	-- multiplication 2
+				'0';
 	
 	-- Flag Zero
-	Z <=  '1' when (Ctrl_Alu = X"1" and S_add(size-1 downto 0) = X"00") else -- addition 1
-			'1' when (Ctrl_Alu = X"3" and S_sou(size-1 downto 0) = X"00") else -- soustraction 3
-			'1' when (Ctrl_Alu = X"2" and S_mul(size-1 downto 0) = X"00") else -- multiplication 2
-			'0';
+	Z_AUX <=  '1' when (Ctrl_Alu = X"1" and S_add(size-1 downto 0) = X"00") else -- addition 1
+				'1' when (Ctrl_Alu = X"3" and S_sou(size-1 downto 0) = X"00") else -- soustraction 3
+				'1' when (Ctrl_Alu = X"2" and S_mul(size-1 downto 0) = X"00") else -- multiplication 2
+				'0';
 	
 	-- Flag Overflow
-	V <=  '1' when (S_add(size-1) = '1' and A(size-1) = '0' and B(size-1) = '0') else 	-- add cas 1 : A et B positifs
+	O_AUX <=  '1' when (S_add(size-1) = '1' and A(size-1) = '0' and B(size-1) = '0') else 	-- add cas 1 : A et B positifs
 			'1' when (S_add(size-1) = '0' and A(size-1) = '1' and B(size-1) = '1') else	-- add cas 2 : A et B negatifs
 			'1' when (S_sou(size-1) = '0' and A(size-1) = '1' and B(size-1) = '0') else	-- sou cas 1 : A negatif, B positif
 			'1' when (S_sou(size-1) = '1' and A(size-1) = '0' and B(size-1) = '1') else 	-- sou cas 2 : A positif, B negatif
 			'0';
+
+	O_flag <= O_AUX;
+	Z_flag <= Z_AUX;
+	N_flag <= N_AUX;
+	C_flag <= C_AUX;
 
 end Behavioral;
 
--- processeur/PSI_processor/test_alu.vhd
+++ processeur/PSI_processor/test_alu.vhd
@@ -1,122 +1,4 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   19:38:23 05/05/2020
--- Design Name:   
--- Module Name:   /home/erpeldin/Documents/4IR/S8/ProjetSystemInfo/processeur/PSI_processor/test_alu.vhd
--- Project Name:  PSI_processor
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: alu
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
-ENTITY test_alu IS
-END test_alu;
- 
-ARCHITECTURE behavior OF test_alu IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT alu
-    PORT(
-         A : IN  std_logic_vector(7 downto 0);
-         B : IN  std_logic_vector(7 downto 0);
-         S : OUT  std_logic_vector(7 downto 0);
-         Ctrl_Alu : IN  std_logic_vector(3 downto 0);
-         C_Flag : OUT  std_logic;
-         Z_Flag : OUT  std_logic;
-         O_Flag : OUT  std_logic;
-         N_Flag : OUT  std_logic
-        );
-    END COMPONENT;
-    
 
-   --Inputs
-   signal SIGNAL_A : std_logic_vector(7 downto 0) := (others => '0');
-   signal SIGNAL_B : std_logic_vector(7 downto 0) := (others => '0');
-   signal SIGNAL_Ctrl_Alu : std_logic_vector(3 downto 0) := (others => '0');
-
- 	--Outputs
-   signal SIGNAL_S : std_logic_vector(7 downto 0);
-   signal SIGNAL_C_Flag : std_logic;
-   signal SIGNAL_Z_Flag : std_logic;
-   signal SIGNAL_O_Flag : std_logic;
-   signal SIGNAL_N_Flag : std_logic;
-   -- No clocks detected in port list. Replace CLK below with 
-   -- appropriate port name 
- 
-   signal CLK : std_logic := '0';
-   constant CLK_period : time := 10 ns;
- 
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: alu PORT MAP (
-          SIGNAL_A,
-          SIGNAL_B,
-          SIGNAL_S,
-          SIGNAL_Ctrl_Alu,
-          SIGNAL_C_Flag,
-          SIGNAL_Z_Flag,
-          SIGNAL_O_Flag,
-          SIGNAL_N_Flag
-        );
-
-   -- Clock process definitions
-   CLK_process :process
-   begin
-		CLK <= '0';
-		wait for CLK_period/2;
-		CLK <= '1';
-		wait for CLK_period/2;
-   end process;
- 
---	SIGNAL_A 			<= (others => (others => '1'));
---	SIGNAL_B 			<= (others => (others => '1'));
---	SIGNAL_Ctrl_Alu 	<= (others => (others => '1'));
---	SIGNAL_S 			<= (others => (others => '1'));
-	
-	SIGNAL_C_Flag <= '0', '1' after 30 ns, '0' after 600 ns;
-	SIGNAL_Z_Flag <= '0', '1' after 250 ns, '0' after 270 ns;
-	SIGNAL_O_Flag <= '0', '1' after 500 ns , '0' after 570 ns;
-	SIGNAL_N_Flag <= '1', '0' after 300 ns;
-
-   -- Stimulus process
---   stim_proc: process
---   begin		
---      -- hold reset state for 100 ns.
---      wait for 100 ns;	
---
---      wait for CLK_period*10;
---
---      -- insert stimulus here 
---
---      wait;
---   end process;
-
-END;--------------------------------------------------------------------------------
 -- Company: 
 -- Engineer:
 --
@@ -169,10 +51,12 @@ ARCHITECTURE behavior OF test_alu IS
          N_Flag : OUT  std_logic
         );
     END COMPONENT;
-    
-
+	
+	  -- Clock period definitions
+   constant CLK_period : time := 50 ns;
+	
    --Inputs
-	--signal CLK : std_logic :='0';
+	signal CLK : std_logic :='0';
    signal A : std_logic_vector(7 downto 0) := (others => '0');
    signal B : std_logic_vector(7 downto 0) := (others => '0');
    signal Ctrl_Alu : std_logic_vector(3 downto 0) := (others => '0');
@@ -199,39 +83,29 @@ BEGIN
         );
 
    -- Clock process definitions
---   CLK_process :process
---   begin
---		CLK <= '0';
---		wait for CLK_period/2;
---		CLK <= '1';
---		wait for CLK_period/2;
---   end process;
--- 
---	A 			<= X"01";
---	B 			<= X"02";
---	Ctrl_Alu <= X"1";
---	S <= S;
-	
+	CLK_process :process
+	begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+	end process;
+ 
 	 -- Stimulus process
-   stim_proc: process	
-   begin		
-      -- hold reset state for 100 ns.
+	 
 		A <= X"01";
 		B <= X"02";
-      wait for 100 ns;	
       Ctrl_Alu <= X"0", X"1" after 100ns, X"2" after 200 ns, X"3" after 300ns;
-      wait;
-   end process;
-
-   -- Stimulus process
+     
+--    Stimulus process
 --   stim_proc: process
 --   begin		
---      -- hold reset state for 100 ns.
+--       hold reset state for 100 ns.
 --      wait for 100 ns;	
 --
 --      wait for CLK_period*10;
 --
---      -- insert stimulus here 
+--       insert stimulus here 
 --
 --      wait;
 --   end process;
